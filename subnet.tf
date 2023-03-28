1800dbc953ea23c0cc52224a1def84c3faa9e236
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-28 18:11:15 +0530

    updating

--- subnet.tf
+++ subnet.tf
@@ -2,8 +2,18 @@ resource "aws_subnet" "public-subnet" {
   count      = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
-
+  availability_zone = element(var.AZ,count.index
   tags = {
     Name = "robot-public-subnet-${element(var.AZ,count.index)}"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  count      = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR,count.index)
+  availability_zone = element(var.AZ,count.index
+  tags = {
+    Name = "robot-private-subnet-${element(var.AZ,count.index)}"
   }
 }
