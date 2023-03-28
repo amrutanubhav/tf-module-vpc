fcfdea0b4f5056028672c04860b3944540cae5f1
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-28 16:53:18 +0530

    updating

--- vars.tf
+++ vars.tf
@@ -1,13 +1,13 @@
 variable "VPC_CIDR" {}
 variable "VPC_CIDR" {}
-variable "DEFAULT_VPC_CIDR" {}
-variable "DEFAULT_VPC_ID" {}
-variable "DEFAULT_VPC_RT" {}
-variable "PRIVATE_HOSTEDZONE_ID" {}
-variable "PUBLIC_HOSTEDZONE_ID" {}
-variable "PRIVATE_HOSTEDZONE_NAME" {}
-variable "PUBLIC_HOSTEDZONE_NAME" {}
+# variable "DEFAULT_VPC_CIDR" {}
+# variable "DEFAULT_VPC_ID" {}
+# variable "DEFAULT_VPC_RT" {}
+# variable "PRIVATE_HOSTEDZONE_ID" {}
+# variable "PUBLIC_HOSTEDZONE_ID" {}
+# variable "PRIVATE_HOSTEDZONE_NAME" {}
+# variable "PUBLIC_HOSTEDZONE_NAME" {}
 variable "ENV" {}
-variable "PUBLIC_SUBNET_CIDR" {}
-variable "PRIVATE_SUBNET_CIDR" {}
-variable "AZ" {}
+# variable "PUBLIC_SUBNET_CIDR" {}
+# variable "PRIVATE_SUBNET_CIDR" {}
+# variable "AZ" {}
