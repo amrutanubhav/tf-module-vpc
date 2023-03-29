a5ebeccb67b5d9441cac34a29053da50781d4fb3
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-29 23:41:50 +0530

    updating

--- route-table.tf
+++ route-table.tf
@@ -17,7 +17,7 @@ resource "aws_route_table" "public-rt" {
 
 resource "aws_route_table_association" "public-rt-association" {
   count             = length(aws_subnet.public-subnet.*.id)
-  subnet_id         = element(length(aws_subnet.public-subnet.*.id), count.index)
+  subnet_id         = element(aws_subnet.public-subnet.*.id, count.index)
   route_table_id    = aws_route_table.public-rt.id
 }
 
@@ -42,7 +42,7 @@ resource "aws_route_table" "private-rt" {
 
 resource "aws_route_table_association" "private-rt-association" {
   count             = length(aws_subnet.private-subnet.*.id)
-  subnet_id         = element(length(aws_subnet.private-subnet.*.id), count.index)
+  subnet_id         = element(aws_subnet.private-subnet.*.id, count.index)
   route_table_id    = aws_route_table.private-rt.id
 }
 
