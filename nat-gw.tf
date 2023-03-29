9f82c9af345b95781a314b59602409b10b8b9e61
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-29 23:18:20 +0530

    updating

---
+++ nat-gw.tf
@@ -0,0 +1,20 @@
+#provisions an elastic Ip needed for nat gateway
+resource "aws_eip" "ngw-aws_eip" {
+  vpc      = true
+  tags = {
+    Name = "robot-${var.ENV}-ngw-elastic-ip"
+  }
+}
+#creates a NAT gateway
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.ngw-aws_eip.id
+  subnet_id     = aws_subnet.public-subnet.*.id[0]
+
+  tags = {
+    Name = "robot-${var.ENV}-ngw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
---
+++ route-table.tf
@@ -0,0 +1,48 @@
+# creating public route table
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "robot-${var.ENV}-public-rt"
+  }
+}
+
+#attach route table to public subnet
+
+resource "aws_route_table_association" "public-rt-association" {
+  count             = length(aws_subnet.public-subnet.*.id)
+  subnet_id         = element(length(aws_subnet.public-subnet.*.id))
+  route_table_id    = aws_route_table.bar.id
+}
+
+
+
+# creating private route table
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = var.VPC_CIDR
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "robot-${var.ENV}-private-rt"
+  }
+}
+
+#attach route table to private subnet
+
+resource "aws_route_table_association" "private-rt-association" {
+  count             = length(aws_subnet.private-subnet.*.id)
+  subnet_id         = element(length(aws_subnet.private-subnet.*.id))
+  route_table_id    = aws_route_table.bar.id
+}
+
