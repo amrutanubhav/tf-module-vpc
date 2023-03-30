47a481d0b3a11dc2661d73a7ae367c42d7ef21d0
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-30 15:30:48 +0530

    updating

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable "VPC_CIDR" {}
-# variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAULT_VPC_CIDR" {}
 variable "DEFAULT_VPC_ID" {}
 # variable "DEFAULT_VPC_RT" {}
 # variable "PRIVATE_HOSTEDZONE_ID" {}
