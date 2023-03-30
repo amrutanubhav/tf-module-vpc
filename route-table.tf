712b7a0a7f618566d4e468161825926eecf711ac
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-30 23:04:23 +0530

    updating

--- route-table.tf
+++ route-table.tf
@@ -7,6 +7,11 @@ resource "aws_route_table" "public-rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+  
+  route {
+    cidr_block = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+  }
 
   tags = {
     Name = "robot-${var.ENV}-public-rt"
