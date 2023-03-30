8405154078520ff2e9e17981e57ec5d503a001dd
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-30 22:42:03 +0530

    updating

--- route-table.tf
+++ route-table.tf
@@ -30,7 +30,7 @@ resource "aws_route_table" "private-rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ngw.id
+    gateway_id = aws_nat_gateway.ngw.id
   }
 
   route {
