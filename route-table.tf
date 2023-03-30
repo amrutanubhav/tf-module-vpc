7a950d42ce785c64938a81f78d916b6ed42c2e49
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-30 14:45:50 +0530

    updating

--- route-table.tf
+++ route-table.tf
@@ -28,10 +28,10 @@ resource "aws_route_table_association" "public-rt-association" {
 resource "aws_route_table" "private-rt" {
   vpc_id = aws_vpc.main.id
 
-#   route {
-#     cidr_block = var.DEFAULT_VPC_CIDR
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
+  route {
+    cidr_block = 0.0.0.0/0
+    gateway_id = aws_internet_gateway.ngw.id
+  }
 
 #   route {
 #     cidr_block = var.DEFAULT_VPC_CIDR
